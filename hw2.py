ece685a1214f2376992295e16cfe8ad79442eb31
Joseph <niceinho@gmail.com>
2022-03-02 18:00:47 -0500

    added main python file

---
+++ hw2.py
@@ -0,0 +1,22 @@
+# Joseph Chang - G01189913
+
+# Regex for String filtering
+import re
+
+# Data Visualization
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+# File paths
+training_file = 
+testing_file = 
+output_file = 
+
+
+# dicts for training
+
+
+def training():
+    
